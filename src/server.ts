d59d7ebe9120721be8b14e9e9e9c9c882687572c
Andy Brassaw <andy.brassaw@rentrakmail.com>
2017-12-09 14:41:03 -0800

    Change binding from localhost to 0.0.0.0

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { routes } from './weather/weather.routes';
 const Hapi = require('hapi');
 
 const server: Server = new Hapi.Server();
-server.connection({ port: 3000, host: 'localhost' });
+server.connection({ port: 3000, host: '0.0.0.0' });
 
 server.route(routes);
 
